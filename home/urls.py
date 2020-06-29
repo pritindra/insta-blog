4ce96679437898af824037ad87f143a932320bfc
pritindra <37242669+pritindra@users.noreply.github.com>
2020-06-29 19:00:36 +0530

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
-from . import views
+from .views import (
+    PostListView,
+    PostDetailView
+)
 urlpatterns = [
-    path('', views.home , name='home')
+    path('', PostListView.as_view(), name='home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail')
 ]
